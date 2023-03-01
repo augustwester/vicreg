4ae6827262386d3bccc8ff8033f1c8589a2d6bbd
August Wester <august.wester@gmail.com>
2023-03-01 15:25:17 +0100

    Add comment to ResNet modification

--- vicreg/model.py
+++ vicreg/model.py
@@ -25,9 +25,16 @@ class Projector(nn.Module):
 class VICReg(nn.Module):
     def __init__(self, encoder_dim, projector_dim):
         super().__init__()
+        
+        # the default ResNet has a 7x7 kernel with stride 2 as its initial
+        # convolutional layer. this works for ImageNet but is too reductive for
+        # CIFAR-10. we follow the SimCLR paper and replace it with a 3x3 kernel
+        # with stride 1 and remove the max pooling layer.
+        
         self.encoder = resnet18(num_classes=encoder_dim)
         self.encoder.conv1 = nn.Conv2d(3, 64, kernel_size=(3,3), stride=1)
         self.encoder.maxpool = nn.Identity()
+        
         self.projector = Projector(encoder_dim, projector_dim)
     
     def forward(self, x1, x2):
